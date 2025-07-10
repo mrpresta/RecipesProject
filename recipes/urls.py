3e739d18950a459aab5e95ccf5dd0210337d9f18
mrpresta <felipepresta@gmial.com>
2025-07-10 01:01:27 -0300

    criação do models primira migração, algumas organizações para separar o conteudo das paginas html e css

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from recipes.views import home
+from . import views
 
 urlpatterns = [
-    path('', home)
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipes, name='recipes_description'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html')
+    return render(request, 'recipes/home.html', context={
+        'recipes': [make_recipe() for _ in range(10)]
+    })
+
+def recipes(request, id):
+    return render(request, 'recipes/recipe_description.html', context={
+        'recipe': make_recipe,
+        'is_datail_page': True,
+    })
